45ef8dbea185ba91f885bf54be938a856cb427c6
Bruno Staub <bruno.staub@gmail.com>
2018-06-30 14:04:54 +0200

    Hero Tutorial 6. Services Part4 messageService (methods add/clear) in hero.service (service-in-service), add messages.component and public inject messageService
    Signed-off-by: Bruno Staub <bruno.staub@gmail.com>

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,14 +4,18 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';   // also import of!
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) {
+  }
 
   getHeroes(): Observable<Hero[]> {   // Async Way, with subscribe Observer later!
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES); // of(HEROES), returns Observable<Hero[]>
   }
 }
