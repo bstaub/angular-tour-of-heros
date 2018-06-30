0f5c1f8c980e16f6e7222c1024d045e54d7d198d
Bruno Staub <bruno.staub@gmail.com>
2018-06-30 17:18:03 +0200

    Hero Tutorial 7. Routing, getHero with paramMap.get('id'), goBack(), HeroService in hero-detail Part3, Routing finished
    Signed-off-by: Bruno Staub <bruno.staub@gmail.com>

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Hero } from '../hero';
 
-import {HeroService} from '../hero.service';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +11,11 @@ import {HeroService} from '../hero.service';
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];  // Achtung hier auf nichts referenzieren, nur Hero[] hinzufügen!
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {
+  }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
   getHeroes(): void {
     this.heroService.getHeroes()
