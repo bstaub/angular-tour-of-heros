795351f03df270c94d7c6e9687f0c3146bfe1474
Bruno Staub <bruno.staub@gmail.com>
2018-06-30 16:30:02 +0200

    Hero Tutorial 7. Routing, add detail/:id with routerLink Part2
    Signed-off-by: Bruno Staub <bruno.staub@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component: HeroDetailComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'heroes', component: HeroesComponent}
 ];
